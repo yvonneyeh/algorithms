c9e272a5bae9cf801292d70364bf50a75f5043e5
Yvonne Yeh <yvonne@hologram.io>
2022-07-29 12:54:59 -0700

    string algos

--- strings/URLify.py
+++ strings/URLify.py
@@ -5,3 +5,26 @@
 def urlify_pythonic(text, length):
     """solution using standard library"""
     return text.rstrip().replace(' ', '%20')
+
+
+def urlify_algo(string, length):
+  """replace spaces with %20 and removes trailing spaces, assuming the trailing spaces were pusposely put there to make sure that the final string fits in them"""
+  # Create an empty array that is sufficiently long
+  char_list = [""] * len(string)
+
+  new_index = 0
+  for i in range(length):
+    next_char = string[i] # Look up in the original string
+    if next_char != " ":
+      # Put the character in the new position
+      char_list[new_index] = next_char
+      new_index += 1
+    else:
+      # Put 3 characters in the new position
+      char_list[new_index] = "%"
+      char_list[new_index + 1] = "2"
+      char_list[new_index + 2] = "0"
+      new_index += 3
+
+  # convert back to string
+  return "".join(char_list[:new_index])
--- strings/is_palindrome.py
+++ strings/is_palindrome.py
@@ -16,3 +16,30 @@ def is_palindrome_(s):
     return True
 # Time complexity: O(n)
 # Auxiliary Space: O(1)
+
+
+# Palindrome permutation:
+
+"""
+Given a string, write a function to check if it is a permutation of a palindrome.
+A palindrome is a word or phrase that is the same forwards and backwards.
+A permutation is a rearrangement of letters.
+The palindrome does not need to be limited to just dictionary words.
+EXAMPLE
+Input: Tact Coa
+Output: True (permutations: "taco cat", "atco cat", etc.)
+
+I: String
+O: Boolean
+C: optimize
+E: empty string, even and odd chars, spaces between and in front and at end, more than 2 of the same char
+time complexity: linear
+space complexity: linear
+"""
+
+def is_palindrome_permutation_pythonic(phrase):
+    counter = Counter(phrase.replace(" ", "").lower())
+    return sum(val % 2 for val in counter.values()) <= 1
+
+def is_palindrome_permutation(s):
+    
