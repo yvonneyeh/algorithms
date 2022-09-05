b81339e3847bf6bb0a3edceb480eb8f4fa211e30
Yvonne Yeh <code@yvonneyeh.com>
2022-09-05 14:29:36 -0700

    Number of Islands

--- graphs/num_islands.py
+++ graphs/num_islands.py
@@ -106,3 +106,42 @@ class DFS:
                         islands += 1
                         dfs(r, c)
             return islands
+
+
+def countIslandsDFS(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    totalIslands = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if (matrix[i][j] == 1):  # only if the cell is a land
+                # we have found an island
+                totalIslands += 1
+                visitIslandDFS(matrix, i, j)
+    return totalIslands
+
+
+def visitIslandDFS(matrix,  x,  y):
+    if (x < 0 or x >= len(matrix) or y < 0 or y >= len(matrix[0])):
+        return  # return, if it is not a valid cell
+    if (matrix[x][y] == 0):
+        return  # return, if it is a water cell
+
+    matrix[x][y] = 0  # mark the cell visited by making it a water cell
+
+    # recursively visit all neighboring cells (horizontally & vertically)
+    visitIslandDFS(matrix, x + 1, y)  # lower cell
+    visitIslandDFS(matrix, x - 1, y)  # upper cell
+    visitIslandDFS(matrix, x, y + 1)  # right cell
+    visitIslandDFS(matrix, x, y - 1)  # left cell
+
+
+def main():
+    print(countIslandsDFS([[0, 1, 1, 1, 0], [0, 0, 0, 1, 1], [
+          0, 1, 1, 1, 0], [0, 1, 1, 0, 0], [0, 0, 0, 0, 0]]))
+    print(countIslandsDFS([[1, 1, 1, 0, 0], [0, 1, 0, 0, 1], [
+          0, 0, 1, 1, 0], [0, 0, 1, 0, 0], [0, 0, 1, 0, 0]]))
+
+
+main()
