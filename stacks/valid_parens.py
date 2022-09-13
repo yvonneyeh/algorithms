0f7cceafefa6d3a9d734fe97f79b89461dde776c
Yvonne Yeh <code@yvonneyeh.com>
2022-09-13 14:14:39 -0700

    Valid Parentheses

--- stacks/valid_parens.py
+++ stacks/valid_parens.py
@@ -36,6 +36,16 @@ def isValid_dict(self, s: str) -> bool:
 
 def isValid_list(self, s: str) -> bool:
 
+    # open and closing lists
+    # init stack
+    # look at every char in s
+    # if char is in open, append to stack
+    # if not, check if there is item in stack and last item matches opening index
+        # pop item if valid
+        # else = not valid
+
+    # return true if len of stack is empty
+
     opened = ['(','{','[']
     closed = [')','}',']']
     stack = []
@@ -50,3 +60,20 @@ def isValid_list(self, s: str) -> bool:
                 return False
 
     return len(stack) == 0
+
+class Solution_dict:
+    def isValid(self, s: str) -> bool:
+
+        # if it's the left bracket then we append it to the stack
+        # else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+        # finally check if the stack still contains unmatched left bracket
+
+        parens = {'(':')', '{':'}','[':']'}
+        stack = []
+
+        for c in s:
+            if c in parens:
+                stack.append(c)
+            elif not stack or parens[stack.pop()] != c:
+                    return False
+        return len(stack) == 0
