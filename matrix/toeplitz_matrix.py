a1067f3b6cde6a09ce8b1c4e4c6a95f7374031a0
Yvonne Yeh <code@yvonneyeh.com>
2022-12-22 14:43:35 -0800

    Toeplitz Matrix

---
+++ matrix/toeplitz_matrix.py
@@ -0,0 +1,9 @@
+"""
+A Toeplitz Matrix is one where the values on every diagonal are the same: Given a 2d matrix (multidimensional array), return true if the input is a Toeplitz matrix and false otherwise.
+
+Example of a valid Toeplitz matrix:
+1 2 3 4
+5 1 2 3
+6 5 1 2
+7 6 5 1
+"""
