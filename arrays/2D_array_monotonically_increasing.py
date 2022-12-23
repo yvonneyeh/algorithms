661120568cb322f115b5d74afb9d150a0101a718
Yvonne Yeh <code@yvonneyeh.com>
2022-12-23 11:49:14 -0800

    2D array: monotonically increasing rows and columns

---
+++ arrays/2D_array_monotonically_increasing.py
@@ -0,0 +1,15 @@
+"""
+Given a rectangular 2D array of integers, return true if all rows and all columns are monotonically increasing. This means that every successive value along all rows and columns must be AT LEAST as large as what came before.
+
+Example:
+[[0, 0, 0, 1],
+ [1, 1, 1, 2],
+ [2, 3, 4, 5]]
+Returns true but this next one returns false.
+[[0, 0, 0, 1],
+ [1, 1, 3, 2],
+ [2, 3, 4, 5]]
+function isMatrixMonotonic(matrix) {
+ /* your code here */
+}
+"""
