a46dade7defca674eabd1825afb7c843a6505a40
Yvonne Yeh <code@yvonneyeh.com>
2022-12-23 11:46:06 -0800

    Toeplitz Matrix

--- matrix/toeplitz_matrix.py
+++ matrix/toeplitz_matrix.py
@@ -7,3 +7,24 @@ Example of a valid Toeplitz matrix:
 6 5 1 2
 7 6 5 1
 """
+
+
+# function isToeplitz(matrix) {
+#   // Here's that row-major traversal again, using two loops.
+#   for (let r = 0; r < matrix.length; r++) {
+#     for (let c = 0; c < matrix[r].length; c++) {
+#       const value = matrix[r][c];
+#
+#       // Here, we're going to loop up and left at a position
+#       // diagonal from this one. Only do this if we're not going
+#       // to index outside of the matrix. Check if that value isn't
+#       // the same as this one.
+#       if (r > 0 && c > 0 && matrix[r - 1][c - 1] !== value) {
+#         return false;
+#       }
+#     }
+#   }
+#
+#   // If we didn't find any problems, return true.
+#   return true;
+# }
