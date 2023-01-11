f1f7b2e3ee8dec7cebe3eee3464def486d40b5f5
Yvonne Yeh <code@yvonneyeh.com>
2023-01-11 11:00:43 -0800

    Python for interviews

---
+++ python.py
@@ -0,0 +1,23 @@
+# Variables are dynamicly typed
+n = 0
+print('n =', n)
+>>> n = 0
+
+n = "abc"
+print('n =', n)
+>>> n = abc
+
+# Multiple assignments
+n, m = 0, "abc"
+n, m, z = 0.125, "abc", False
+
+# Increment
+n = n + 1 # good
+n += 1    # good
+n++       # bad
+
+# None is null (absence of value)
+n = 4
+n = None
+print("n =", n)
+>>> n = None
