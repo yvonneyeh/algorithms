74b5e491c29f4e6358650c93fd259e642d6ebd23
Yvonne Yeh <code@yvonneyeh.com>
2023-01-12 10:18:56 -0800

    Python for interviews

--- python.py
+++ python.py
@@ -21,3 +21,231 @@ n = 4
 n = None
 print("n =", n)
 >>> n = None
+
+
+# If statements don't need parentheses
+# or curly braces.
+n = 1
+if n > 2:
+    n -= 1
+elif n == 2:
+    n *= 2
+else:
+    n += 2
+
+# Parentheses needed for multi-line conditions.
+# and = &&
+# or  = ||
+n, m = 1, 2
+if ((n > 2 and
+    n != m) or n == m):
+    n += 1
+
+
+n = 0
+while n < 5:
+    print(n)
+    n += 1
+>>> 0 1 2 3 4
+
+# Looping from i = 0 to i = 4
+for i in range(5):
+    print(i)
+>>> 0 1 2 3 4
+
+# Looping from i = 2 to i = 5
+for i in range(2, 6):
+    print(i)
+>>> 2 3 4 5
+
+# Looping from i = 5 to i = 2
+for i in range(5, 1, -1):
+    print(i)
+>>> 5 4 3 2
+
+# Division is decimal by default
+print(5 / 2)
+>>> 2.5
+
+# Double slash rounds down
+print(5 // 2)
+>>> 2
+
+# CAREFUL: most languages round towards 0 by default
+# So negative numbers will round down
+print(-3 // 2)
+>>> -2
+
+# A workaround for rounding towards zero
+# is to use decimal division and then convert to int.
+print(int(-3 / 2))
+>>> -1
+
+# Modding is similar to most languages
+print(10 % 3)
+>>> 1
+
+# Except for negative values
+print(-10 % 3)
+>>> 2
+
+# To be consistent with other languages modulo
+import math
+from multiprocessing import heap
+print(math.fmod(-10, 3))
+>>> -1
+
+# More math helpers
+print(math.floor(3 / 2))
+>>> 1
+print(math.ceil(3 / 2))
+>>> 2
+print(math.sqrt(2))
+>>> 1.4142135623730951
+print(math.pow(2, 3))
+>>> 8
+
+# Max / Min Int
+float("inf")
+float("-inf")
+
+# Python numbers are infinite so they never overflow
+print(math.pow(2, 200))
+>>> 1.6069380442589903e+60
+
+# But still less than infinity
+print(math.pow(2, 200) < float("inf"))
+>>> True
+
+
+# Arrays (called lists in python)
+arr = [1, 2, 3]
+print(arr)
+>>> [1, 2, 3]
+
+# Can be used as a stack
+arr.append(4)
+arr.append(5)
+print(arr)
+>>> [1, 2, 3, 4, 5]
+
+arr.pop()
+print(arr)
+>>> [1, 2, 3, 4]
+
+arr.insert(1, 7)
+print(arr)
+>>> [1, 7, 2, 3, 4]
+
+arr[0] = 0
+arr[3] = 0
+print(arr)
+>>> [0, 7, 2, 0, 4]
+
+# Initialize arr of size n with default value of 1
+n = 5
+arr = [1] * n
+print(arr)
+>>> [1, 1, 1, 1, 1]
+print(len(arr))
+>>> 5
+
+# Careful: -1 is not out of bounds, it's the last value
+arr = [1, 2, 3]
+print(arr[-1])
+>>> 3
+
+# Indexing -2 is the second to last value, etc.
+print(arr[-2])
+>>> 2
+
+# Sublists (aka slicing)
+arr = [1, 2, 3, 4]
+print(arr[1:3])
+>>> [2, 3]
+
+# Similar to for-loop ranges, last index is non-inclusive
+print(arr[0:4])
+>>> [1, 2, 3, 4]
+
+# But no out of bounds error
+print(arr[0:10])
+>>> [1, 2, 3, 4]
+
+# Unpacking
+a, b, c = [1, 2, 3]
+print(a, b, c)
+>>> 1, 2, 3
+
+# Be careful though, this throws an error
+a, b = [1, 2, 3]
+
+# Looping through arrays
+nums = [1, 2, 3]
+
+# Using index
+for i in range(len(nums)):
+    print(nums[i])
+>>> 1 2 3
+
+# Without index
+for n in nums:
+    print(n)
+>>> 1 2 3
+
+# With index and value
+for i, n in enumerate(nums):
+    print(i, n)
+>>> 0 1
+>>> 1 2
+>>> 2 3
+
+# Loop through multiple arrays simultaneously with unpacking
+nums1 = [1, 3, 5]
+nums2 = [2, 4, 6]
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+>>> 1 2
+>>> 3 4
+>>> 5 6
+
+# Reverse
+nums = [1, 2, 3]
+nums.reverse()
+print(nums)
+>>> [3, 2, 1]
+
+
+# Sorting
+arr = [5, 4, 7, 3, 8]
+arr.sort()
+print(arr)
+>>> [3, 4, 5, 7, 8]
+
+arr.sort(reverse=True)
+print(arr)
+>>> [8, 7, 5, 4, 3]
+
+arr = ["bob", "alice", "jane", "doe"]
+arr.sort()
+print(arr)
+>>> ["alice", "bob", "doe", "jane"]
+
+# Custom sort (by length of string)
+arr.sort(key=lambda x: len(x))
+print(arr)
+>>> ["bob", "doe", "jane", "alice"]
+
+# List comprehension
+arr = [i for i in range(5)]
+print(arr)
+>>> [0, 1, 2, 3, 4]
+
+# 2-D lists
+arr = [[0] * 4 for i in range(4)]
+print(arr)
+print(arr[0][0], arr[3][3])
+>>> [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+# This won't work as you expect it to
+arr = [[0] * 4] * 4
