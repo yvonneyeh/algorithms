dc0464dd78f88cb93532727b1f630d3a8903830d
Yvonne Yeh <code@yvonneyeh.com>
2023-02-02 21:43:36 -0800

    Check if linked list is palindrome

--- linked_lists/palindrome_linkedlist.py
+++ linked_lists/palindrome_linkedlist.py
@@ -6,6 +6,39 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        current = head
+        arr = []
+
+        while current:
+            arr.append(current.val)
+            current = current.next
+
+        l = 0
+        r = len(arr) - 1
+        while l < r:
+            if arr[l] != arr[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+        # mid = len(arr) // 2
+
+        # print(arr[:mid], arr[mid::-1])
+
+        # return arr[:mid] == arr[mid:]
+
+
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         slow = fast = head
