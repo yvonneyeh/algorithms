d53a3d8c75ef0f923541a90e039e03b7207e3e78
Yvonne Yeh <code@yvonneyeh.com>
2023-02-08 12:38:56 -0800

    Linked list drills

--- linked_lists/drills.py
+++ linked_lists/drills.py
@@ -1,3 +1,261 @@
+"""Q. Given two linked lists of different length, sum elements' value at the same position.
+
+Example:
+Input: LL1: 1->2->5, LL2: 3->4
+Output: 4->6->5
+[execution time limit] 4 seconds (py3)
+
+[input] linkedlist.integer head1
+
+head of the list1
+
+[input] linkedlist.integer head2
+
+head of the list2
+
+[output] linkedlist.integer
+
+head of the summed list"""
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head1, head2):
+
+    if not head1 or not head2:
+        return None
+
+    dummy = ListNode(0)
+    dummy.next = head1
+
+    while head1 and head2:
+        head1.value += head2.value
+        head1 = head1.next
+        head2 = head2.next
+
+    return dummy.next
+
+"""
+Case 1:
+Input:
+head1: []
+head2: [1]
+Output:
+[]
+Expected Output:
+[1]
+
+
+Case 2:
+Input:
+head1: [2, 4]
+head2: [1, 2, 3]
+Output:
+[3, 6]
+Expected Output:
+[3, 6, 3]
+"""
+
+"""
+Q. Given an unsorted linked list with unique values, insert an element before the target element
+
+If target cannot be found in the list, do nothing.
+Example:
+
+Given a linked list: 3 -> -1 -> 2 -> 5, element (to be inserted): 0, target: 2
+// returns: 3 -> -1 -> 0 -> 2 -> 5
+[execution time limit] 4 seconds (py3)
+
+[input] linkedlist.integer head
+
+head of the list
+
+[input] integer target
+
+vale of the target node
+
+[input] integer value
+
+value of the node to be inserted before the target node
+
+[output] linkedlist.integer
+
+head of the updated list
+"""
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head, target, value):
+
+    if not head or not target or not value:
+        return None
+
+    dummy = ListNode(0)
+    dummy.next = current = head
+    previous = dummy
+
+    while current:
+        if current.value == target:
+            new = ListNode(value)
+            new.next = current
+            previous.next = new
+        previous = current
+        current = current.next
+
+    return dummy.next
+
+"""
+Input:
+head: [3, -1, 2, 5]
+target: 2
+value: 0
+Output:
+[]
+Expected Output:
+[3, -1, 0, 2, 5]
+"""
+
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head):
+
+    if not head:
+        return False
+
+    previous = head.value
+    current = head
+    while current:
+        if current.value < previous:
+            return False
+        previous = current.value
+        current = current.next
+
+    return True
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head1, head2):
+
+    if not head1 or not head2:
+        return None
+
+    dummy = ListNode(0)
+    dummy.next = head1
+
+    h1_len = 0
+    h2_len = 0
+
+    while head1:
+        h1_len += 1
+        head1 = head1.next
+
+    while head1:
+        h1_len += 1
+        head1 = head1.next
+
+    if h1_len > h2_len:
+        while head1:
+            head1.value += head2.value
+            head1 = head1.next
+            head2 = head2.next
+
+
+    while head1 and head2:
+        head1.value += head2.value
+        head1 = head1.next
+        head2 = head2.next
+
+    return head1.next
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head):
+
+    if not head:
+        return None
+
+    slow = fast = head
+
+    while fast and fast.next.next.next:
+        slow = slow.next
+        fast = fast.next.next.next
+
+    return slow.value
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head, k):
+
+    if not head:
+        return None
+
+    order = 1
+    result = ListNode(-1)
+
+    while head:
+        order += 1
+        if order == k:
+            result = head
+        head = head.next
+
+    return result
+
+
+# Singly-linked lists are already defined with this interface:
+# class ListNode(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.next = None
+#
+def solution(head, target, value):
+
+    if not head or not target or not value:
+        return None
+
+    dummy = ListNode(0)
+    dummy.next = current = head
+    previous = dummy
+
+    while current:
+        if current.value == target:
+            original_next = current.next
+            current = ListNode(value)
+            previous.next = current
+            current.next = original_next
+        previous = current
+        current = current.next
+
+    return dummy.next
+
+
 """Q. Given a linked list, limit the number of repetitions to k. Iterate through the linked list, keeping track of how many times the value has been repeated. Once a value has been repeated k target number of times, remove any future instances of that node.
 
 [execution time limit] 4 seconds (py3)
@@ -172,6 +430,8 @@ def solution(k):
         current_value = prev.value
 
     return head
+
+# WIP
 
 """
 Q. Given a linked list, determine if it is monotonically increasing. Monotonically increasing means always increasing or remaining constant.
---
+++ recursion/min_max_array_index.py
@@ -0,0 +1,45 @@
+'''
+❓ PROMPT
+Given an array, write 2 recursive functions to find the index of the minimum and maximum element in an array. If there's a tie-breaker, return the first occurrence.
+
+Example(s)
+findMinIndex([12, 1234, 45, 67, 1]) == 4
+findMinIndex([10, 20, 30]) == 0
+findMinIndex([8, 6, 7, 5, 3, 7]) == 4
+
+findMaxIndex([12, 1234, 45, 67, 1]) == 1
+findMaxIndex([10, 20, 30]) == 2
+findMaxIndex([8, 6, 7, 5, 3, 7]) == 0
+
+
+🔎 EXPLORE
+State your assumptions & discoveries:
+
+
+Create examples & test cases:
+
+
+🧠 BRAINSTORM
+What approaches could work? Consider data structures or algorithmic patterns.
+Analyze the space & time complexity.
+Approach 1:
+Time: O()
+Space: O()
+
+
+📆 PLAN
+High-level outline of approach #:
+
+
+🛠️ IMPLEMENT
+function findMinIndex(array) {
+function findMaxIndex(array) {
+def findMinIndex(arr: list[int]) -> int:
+def findMaxIndex(arr: list[int]) -> int:
+
+
+🧪 VERIFY
+Run your examples & test cases.
+Methodically analyze and debug issue(s).
+
+'''
