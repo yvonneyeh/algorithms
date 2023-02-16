4c9f1b7a86002f3d3e6e8dd608863999b46c1f58
Yvonne Yeh <code@yvonneyeh.com>
2023-02-15 20:34:18 -0800

    Insert node in sorted linked list

---
+++ linked_lists/insert_correct_position.py
@@ -0,0 +1,57 @@
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+✏️ Description
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+Q. Given a sorted linked list, insert an element in the appropriate position.
+
+Examples:
+• Given a linked list: 1 ➞ 3 ➞ 4, target: 2 // returns 1 ➞ 2 ➞ 3 ➞ 4
+• Given an empty linked list, target: 1  // returns 1
+
+▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
+🟦 Python
+▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔
+class ListNode:
+    def __init__(self, value = 0, next = None):
+        self.value = value
+        self.next = next
+
+def arrayify(head) -> [int]:
+    array = []
+    ptr = head
+    while ptr != None:
+        array.append(ptr.value)
+        ptr = ptr.next
+    return array
+
+def insert(head: ListNode, target: int) -> ListNode:
+    # Write your code here.
+    dummy = ListNode(0, head)
+    prev = dummy
+    current = head
+    new = ListNode(target)
+
+    while current:
+        print("current.value > target", current.value, ">", target, current.value > target)
+        # if prev.value == 0 and current.value > target:
+        #     print("is this ever happening?")
+        #     original_next = head.next
+        #     dummy.next = new
+        #     new.next = original_next
+
+        if current.value > target and prev.value < target:
+            print("2")
+            original_next = head.next
+            prev.next = new
+            new.next = original_next
+        prev = current
+        current = current.next
+
+    return dummy.next
+
+# Test Cases
+LL1 = ListNode(1, ListNode(3, ListNode(4)))
+LL2 = ListNode(-3, ListNode(-2, ListNode(-1)))
+print(arrayify(insert(LL1, 2))) # [1, 2, 3, 4]
+print(arrayify(insert(LL2, -4))) # [-4, -3, -2, -1]
+print(arrayify(insert(LL2, 0))) # [-3, -2, -1, 0]
+print(arrayify(insert(None, 1))) # [1]
